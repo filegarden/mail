9b732c93d96390d60ea5b20368cad2da2db47650
Grant Gryczan <grantgryczan@gmail.com>
2024-03-15 19:40:07 -0400

    Add `aliases` file

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # Exit if an error occurs or an unset variable is referenced.
 set -eu
 
+# Build our `etc/postfix/aliases` file so Postfix can use it.
+newaliases
+
 # Create an initial DKIM private key, or else OpenDKIM won't start.
 /usr/local/bin/dkim
